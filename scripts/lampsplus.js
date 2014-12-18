e4e91dbf4bbbb7da7ba79dbd7bdeb95f8a696a03
mastermalone <mastermalone@mastermalonebitfenix.(none)>
2014-12-17 22:44:32 -0800

    Uploading test project

---
+++ scripts/lampsplus.js
@@ -0,0 +1,15 @@
+/**
+ * @author mastermalone
+ */
+
+(function(){
+	var LampsUtils = {
+		test: function(){
+			setObjects();
+			var randomArr = new RandomArray();
+			console.log("Value from test", randomArr);
+		}
+	};
+	
+	LampsUtils.test();
+})();
